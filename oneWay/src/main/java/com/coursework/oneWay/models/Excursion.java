14d99ec231fc30194e5c86cf6608a706cb29da2a
Yalama Stanislav <stanislav.yalama@gmail.com>
2022-09-26 01:53:51 +0300

    add excursion.class

--- oneWay/src/main/java/com/coursework/oneWay/models/Excursion.java
+++ oneWay/src/main/java/com/coursework/oneWay/models/Excursion.java
@@ -1,29 +1,16 @@
-//package com.coursework.oneWay.models;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name = "excursion")
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Excursion {
-//    @Id
-//    @Column(name = "id")
-//    private int id;
-//    @Column(name = "location_id")
-//    private int locationId;
-//    @Column(name = "content_type")
-//    private String contentType;
-//    @Column(name = "count_places")
-//    private int countPlaces;
-//    @Column(name = "place_name")
-//    private String placeName;
-//}
+package com.coursework.oneWay.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Excursion {
+    private int id;
+    private int locationId;
+    private String contentType;
+    private int countPlaces;
+    private String placeName;
+}
